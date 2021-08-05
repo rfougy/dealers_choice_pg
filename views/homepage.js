fd4205bf539c75f72caea98a4ac893ae3ff79ef1
Riviere Fougy <rivierefougy@gmail.com>
2021-08-05 16:39:07 -0400

    added pub-cover-images directory, added styling

--- views/homepage.js
+++ views/homepage.js
@@ -1,19 +1,22 @@
 module.exports = (data) => `<DOCTYPE! html>
 <html>
 <head>
-  <link rel="stylsheet" href="/style.css"/>
+  <link rel="stylesheet" href="/style.css"/>
 </head>
 <body>
   <div>
-  <h1>A.PDF TEST GROUNDS</h1>
+  <h1><a href="/">< A.PDF TESTING GROUNDS ></a></h1>
   </div>
   <div>
   <ul>
   ${data.map(singleData => {
-    return `<li>
+    return `<div class="publication-preview">
+    <li class="single-publication">
+    <img src="/pub-cover-images/${singleData.id}.jpg" />
       <h2>${singleData.name}<h2>
       <button type="button"><a href="/publications/${singleData.id}">Learn More</a></button>
     </li>
+    <div>
     `;
   }).join('')}
   </ul>
--- views/publication-details.js
+++ views/publication-details.js
@@ -1,20 +1,21 @@
 module.exports = (singleData) => `<DOCTYPE! html>
 <html>
 <head>
-  <link rel="stylsheet" href="/style.css"/>
+  <link rel="stylesheet" href="/style.css"/>
 </head>
 <body>
   <div>
-  <h1>A.PDF TEST GROUNDS</h1>
+  <h1><a href="/">< A.PDF TESTING GROUNDS ></a></h1>
   </div>
   <div>
   <ul>
     <li>
+      <img src="/pub-cover-images/${singleData.id}.jpg" />
       <h2>${singleData.name}<h2>
       <h4>Designer: ${singleData.designer}<h4>
       <h4>Date Published: ${singleData.publishing_date}<h4>
       <p>${singleData.description}<p>
-      <button type="button"><a href="/publications">View All Publications</a></button>
+      <button type="button"><a href="/publications">Back to Publications</a></button>
     </li>
   </ul>
   </div>
