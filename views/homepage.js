6222eabea58dba0fdb8f21bc42c4ad7540c311c6
Riviere Fougy <rivierefougy@gmail.com>
2021-08-05 21:55:46 -0400

    fixed div issue via homepage.js, added flex features via style.css

--- views/homepage.js
+++ views/homepage.js
@@ -8,15 +8,15 @@ module.exports = (data) => `<DOCTYPE! html>
   <h1><a href="/">< A.PDF TESTING GROUNDS ></a></h1>
   </div>
   <div>
-  <ul>
+  <ul class="publication-preview">
   ${data.map(singleData => {
-    return `<div class="publication-preview">
-    <li class="single-publication">
+    return `<div class="single-publication">
+    <li>
     <img src="/pub-cover-images/${singleData.id}.jpg" />
       <h2>${singleData.name}<h2>
       <button type="button"><a href="/publications/${singleData.id}">Learn More</a></button>
     </li>
-    <div>
+    </div>
     `;
   }).join('')}
   </ul>
